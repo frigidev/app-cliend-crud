c32fe4ad7306c7b2d0dc372c92fa7ecf9b6566da
Orestes Lucas <oresteslucas@gmail.com>
2024-05-18 23:20:01 -0300

    Update users-form-page.component.ts

--- src/app/pages/users-form-page/users-form-page.component.ts
+++ src/app/pages/users-form-page/users-form-page.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { RandomString } from '../../services/random-string/random-string.service';
 import { Users } from '../../model/users/users';
 import { UsersService } from '../../services/users/users.service';
 import { createValidateCpf, createPasswordStrength } from '../../validators';
@@ -20,17 +19,13 @@ export class UsersFormPageComponent {
   errorMessagePassword = "Your password must contain between 6 or 20 characters, a lowercase letter, an uppercase letter, numbers and at least one symbol";
   errorMessageCPF = "Enter a valid CPF";
 
-  id: string = this.randomString.generateRandomId();
-
   constructor(
     private formBuilder: FormBuilder,
     private usersService: UsersService,
-    private router: Router,
-    private randomString: RandomString
+    private router: Router
   ){}
 
   formGroup = this.formBuilder.group({
-    id: [this.id],
     name: ['', Validators.compose([Validators.pattern(/^[A-Z][A-Za-z(\s)?]{3,30}$/), Validators.required])],
     email: ['', Validators.compose([Validators.pattern(/^([\w].+)@([\w]{2,15}).([\w]{2,10})$/), Validators.required])],
     password: ['', Validators.compose([createPasswordStrength(), Validators.required])],
@@ -49,7 +44,6 @@ export class UsersFormPageComponent {
 
   formToValue(form: typeof this.formGroup): Users {
     return {
-      id: form.value.id!,
       name: form.value.name!,
       email: form.value.email!,
       password: form.value.password!,
@@ -57,7 +51,7 @@ export class UsersFormPageComponent {
     }
   }
 
-  error(control: 'id' |  'name' | 'email' | 'cpf' | 'password', validator: string){
+  error(control: 'name' | 'email' | 'cpf' | 'password', validator: string){
     return this.formGroup.controls[control].getError(validator) ? true : false;
   }
 }
